ee21e8c2eeb7a11c0fb1ead1505e5f5721dbb6ac
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-03-31 22:49:35 -0300

    Add preliminary error handling.

--- tidyr.lisp
+++ tidyr.lisp
@@ -17,15 +17,17 @@
     (declare (ignore d))
     filename))
 
-(defun with-configuration-file-lines (callback)
-  (ignore-errors
+(defun with-configuration-file-lines (callback on-error)
+  (handler-case
     (dolist (line (uiop:read-file-lines (uiop:native-namestring "~/.tidyr")))
       (let* ((parts (str:split " " line))
               (source-dir (uiop:directory-exists-p (nth 0 parts)))
               (extension (nth 1 parts))
               (destination-dir (uiop:directory-exists-p (nth 2 parts))))
         (if (and (= (length parts) 3) source-dir destination-dir)
-          (funcall callback source-dir extension destination-dir))))))
+          (funcall callback source-dir extension destination-dir))))
+    (error (e)
+      (funcall on-error e))))
 
 (defun tidyr-main ()
   (loop
@@ -35,7 +37,10 @@
         (format t "Read configuration line. Source: ~a Extension: ~a Destination: ~a~&" source-dir extension destination-dir)
         (dolist (source-fpath (find-files source-dir extension))
           (let ((destination-fpath (filepath-format destination-dir (filepath-only-filename source-fpath))))
-            (format t "~a -> ~a~&" source-fpath destination-fpath)
-            (uiop:rename-file-overwriting-target source-fpath destination-fpath)))))
+            (progn
+              (format t "~a -> ~a~&" source-fpath destination-fpath)
+              (uiop:rename-file-overwriting-target source-fpath destination-fpath)))))
+      (lambda (e)
+        (format t "Error: ~a~&" e)))
     (finish-output nil)
     (sleep 10)))
